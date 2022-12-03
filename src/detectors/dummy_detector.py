bfbf49f3628cba66224240619a033a4dde395f88
Stanly Samuel <stanlyjohnsamuel@yahoo.com>
2022-12-04 04:01:14 +0530

    Added detector vscode extension template

---
+++ src/detectors/dummy_detector.py
@@ -0,0 +1,6 @@
+import sys
+
+# Code to take as input a solidity program and print it's contents
+f = open(sys.argv[1])
+print("Yes/No")
+print(f.read())
--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from "child_process";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ethguard" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ethguard.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('ethguard.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from EthGuard!');
+		const editor = vscode.window.activeTextEditor?.document.fileName;
+		// editor.document.save();
+
+		const execShell = (cmd: string) =>
+			new Promise<string>((resolve, reject) => {
+				cp.exec(cmd, (err, out) => {
+					if (err) {
+						return reject(err);
+					}
+					return resolve(out);
+				});
+    	});
+		const result = await execShell('python3 /Users/stanly/Project/Veridise/redetector/code/peculiar_dummy.py ' + editor);
+		vscode.window.showInformationMessage(result);
 	});
 
 	context.subscriptions.push(disposable);
