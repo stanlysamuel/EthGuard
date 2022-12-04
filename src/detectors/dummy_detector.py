b21b55d8f1a93679b40ecf06ae2f016cd49c9a6b
veriAjinkya <110429948+veriAjinkya@users.noreply.github.com>
2022-12-04 08:54:01 +0530

    Update dummy_detector.py

--- src/detectors/dummy_detector.py
+++ src/detectors/dummy_detector.py
@@ -1,6 +1,7 @@
 import sys
+from Peculiar.predict import predict
 
 # Code to take as input a solidity program and print it's contents
-# f = open(sys.argv[1])
-print("Yes/No")
+f = open(sys.argv[2])
+predict(f.name)
 # print(f.read())
